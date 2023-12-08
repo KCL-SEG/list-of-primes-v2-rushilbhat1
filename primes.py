0fe51fbcf4e7fb24a66cffc438394da7d2d91f16
rushilbhat1 <bhatrushil@outlook.com>
2023-12-08 10:28:04 +0000

    Define primes and helper function isPrime

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def isPrime(num):
+    for i in range (2, math.floor(num**0.5)+1):
+        if num%i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError("Invalid input")
+    
+    num=2
+    while len(list) < number_of_primes:
+        if (isPrime(num)):
+            list.append(num)
+        num += 1
+
     return list
